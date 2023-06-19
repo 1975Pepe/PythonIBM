309efa2ca491ac4c7d7fa5032f56421a246acb98
1975Pepe <jcpervillar@gmail.com>
2023-06-19 20:11:09 +0200

    V1.8

---
+++ Ejercicio_varios_resueltos.py
@@ -0,0 +1,20 @@
+# VARIABLES
+# Lo ideal es declara e inicializar siempre las variables.
+# -----------------------------------------------------------
+# En Python podemos asignar una variable a otra variable diferente.
+var = "Hola mundo"
+var2 = var
+print(var2)
+# la variable var y var2 apuntan a la misma cadena de texto Hola mundo
+# Los nombres de las variables en Python pueden tener cualquier longitud y
+# pueden consistir en letras mayúsculas y minúsculas (A-Z, a-z), dígitos (0-9) y
+# el carácter de subrayado (_). Cualquier otro carácter dará error:
+var& = "Hola mundo"
+# Aunque el nombre de una variable puede contener dígitos, el primer carácter de
+# un nombre de variable no puede ser un dígito.
+2var = "Hola mundo"
+# El nombre de las variables en Python es sensible a mayúsculas y minúsculas
+Var3 = "Hola mundo"
+print(var3)  # Error, no se encuentra var3
+# Declaración de variable numérica entera:
+n_edad = 47
