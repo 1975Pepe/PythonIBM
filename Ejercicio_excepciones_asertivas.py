246bdeb560efa6d7fd37a02ee5f15d9e2f8e992f
1975Pepe <jcpervillar@gmail.com>
2023-07-02 13:41:56 +0200

    V1.18

---
+++ Ejercicio_excepciones_asertivas.py
@@ -0,0 +1,10 @@
+
+assert 5 == 5  # Success Example
+resultado = 5 == 5
+assert resultado
+print(resultado)
+
+# assert 5 == 3  # Fail Example
+
+assert type(5) is int  # Success Example
+# assert type(5) is not int  # Fail Example
